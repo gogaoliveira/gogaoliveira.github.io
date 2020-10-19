18d915b52ba0394c8485da006a3114940311d90f
Douglas Oliveira <2doug077@gmail.com>
2020-10-19 20:14:55 -0300

    quiz 04

--- js/app.js
+++ js/app.js
@@ -48,5 +48,131 @@ linkNavs.forEach(function(currentValue, index, array){
 
 })
 
+//Quiz
+//Array of objects
+const quiz = [
+    {
+        q : 'Qual a Cidade Natal do Goga?',
+        options : ['Frankfurt', 'Ararangua', 'Hong Kong', 'Ulan Bator'],
+        answer : 1
+    },
+    {
+        q : 'Quantos Anos tem o Goga',
+        options : ['89', '69', '78', '29'],
+        answer : 3
+    },
+    {
+        q : 'Qual o lazer preferido do Goga',
+        options : ['Jogos Eletronicos', 'nadar em rio congelado', 'Caçar Javali', 'Pentiar Macaco'],
+        answer : 0
+    },
+    {
+        q : 'Comida Preferida do Goga',
+        options : ['plastico', 'papel', 'Sushi', 'Vidro'],
+        answer : 2
+    },
+    {
+        q : 'Amor da Vida do Goga',
+        options : ['Flavia', 'Ana Paulo Arosio', 'Hermione', 'Vera Verão'],
+        answer : 0
+    },
+]
+
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector(".question-text");
+const optionContainer = document.querySelector(".option-container");
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+
+function setAvaibleQuestions(){
+    const totalQuestion = quiz.length;
+    for(let i=0; i<totalQuestion; i++){
+       availableQuestions.push(quiz[i])
+    }
+}
+
+function getNewQuestion(){
+    //set question number
+    questionNumber.innerHTML = "Pergunta " + (questionCounter + 1) + " de " + quiz.length; 
+    //set question text
+
+    //get randon question
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+
+    const index1 = availableQuestions.indexOf(questionIndex);
+
+    availableQuestions.splice(index1,1);
+
+    const optionLen = currentQuestion.options.length
+    
+    for(let i=0; i<optionLen; i++){
+        availableOptions.push(i)
+    }
+
+    optionContainer.innerHTML='';
+    
+    let animationDelay = 0.1;
+    //create options i innerHTML
+
+    for (let i=0; i<optionLen; i++) {
+        //randon option
+        const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+        
+        const index2 = availableOptions.indexOf(optionIndex);
+        
+        availableOptions.splice(index2,1)
+
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[optionIndex];
+        option.id = optionIndex;
+        option.style.animationDelay = animationDelay + 's';
+        animationDelay = animationDelay + 0.15;
+        option.className = "option";
+        optionContainer.appendChild(option);
+        option.setAttribute("onclick","getResult(this)");
+        
+    }
+    questionCounter++
+}
+
+function getResult(element){
+    const id = parseInt(element.id);
+    if(id === currentQuestion.answer){
+        element.classList.add("correct");
+    }else{
+        element.classList.add("wrong");
+    }
+
+    unclickableOptions();   
+}
+
+function unclickableOptions(){
+    const optionLen = optionContainer.children.length;
+    for (let i = 0; i < optionLen; i++) {
+        optionContainer.children[i].classList.add("already-answered");
+        
+    }
+}
+
+function next(){
+    if (questionCounter === quiz.length) {
+        console.log("quiz over")
+    }else{
+        getNewQuestion();
+    }
+}
+
+window.onload = function(){
+    setAvaibleQuestions();
+    getNewQuestion();
+}
+
+
+
 
 
