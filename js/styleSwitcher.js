6eb71b0a6008ac695faa3fc0d83a134c067a2fb3
Douglas Oliveira <2doug077@gmail.com>
2020-11-26 19:10:15 -0300

    edit color part 2

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -3,7 +3,7 @@ const links = document.querySelectorAll(".alternate-style"),
 
 function setActiveStyle(color){
     for (let i = 0; i < totalLinks; i++) {
-        if (color == links[i].getAttribute("title")) {
+        if (color === links[i].getAttribute("title")) {
             links[i].removeAttribute("disabled");
         }else{
             links[i].setAttribute("disabled","true");
@@ -12,3 +12,23 @@ function setActiveStyle(color){
     }
 
 }
+
+// body skin
+
+const bodySkin = document.querySelectorAll(".body-skin"),
+      totalBodySkin = bodySkin.length;
+
+      for (let i = 0; i < totalBodySkin; i++) {
+          bodySkin[i].addEventListener("change",function(){
+              if (this.value === "dark") {
+                document.body.className="dark";
+              }else{
+                document.body.className="";
+              }
+          });
+          
+      }
+
+document.querySelector(".toggle-style-switcher").addEventListener("click",() =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
